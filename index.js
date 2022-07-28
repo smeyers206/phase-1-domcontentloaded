1700f403e4b19bf94cfc02db8561a00e2ca6eeae
Shane Meyers <smeyers1031@gmail.com>
2022-07-28 03:20:29 -0700

    commit tree 12cbdcecbae2a691ddcb5813477843c45f7239ac

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const pWIthIdText = document.querySelector("p#text");
+  pWIthIdText.textContent = "This is really cool!";
+});
